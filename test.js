6c0435a3c8313c1d3ebf21c8f648cf620948a96e
firdovsi <megatron_mega@mail.ru>
2024-12-01 02:06:02 +0400

    Fix

---
+++ test.js
@@ -0,0 +1,4 @@
+<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="name@example.com" />
+</Form.Group>
